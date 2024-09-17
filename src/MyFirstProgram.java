886c6c5fce7f74428c1dbf421aa63a7ae4dff940
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2024-09-17 14:54:04 -0400

    lecture 01

---
+++ src/MyFirstProgram.java
@@ -0,0 +1,13 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+
+    }
+}
+/*
+// public (visible or access modifier): accessible everywhere
+// if we have static keyword before any method then it can be called without using objects
+// void: the method is not returning any value
+// main is the name of the method (it is entry point of java program no matter your code is of 4 line or 4000)
+// Command Line ARGUMENTS: String[] args
+*/
---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Naming convention of variables/identifiers in Java
+
+        // 1. It must not start with a number:
+        //int 1a=10; // error
+        // 2. It must not contain white spaces (normal Spaces)
+         // erro int my name  is Gabriel; // u can use pascalCaseConvention or underscores as separators.
+         // int myNameIsGabriel; // correct
+            int my_Name_Is_Gabriel; // correct
+        // 3. Java is a case-sensitive language.
+         // int Gabriel; int gabriel; int GAbriel; int gABriel;
+
+        // 4. It must not be a reserved keyword.
+         // int int; // error
+          int Int; // not giving an error however not a good programming practice.
+        // 5. Can have numbers, underscores if above conditions are satisfied
+
+        // Which following option (s) is/are correct naming concention in Java:
+        // a) 1abc b) _23a c) abc123 d) static e) age
+        // int 1abc; error bcoz starting from number.
+
+
+    }
+}
